15b0cd18c06563062dd766fb4cf8d96bfc33f3a3
juanchomarengo <juanmmarengo@gmail.com>
2023-04-13 20:38:18 -0300

    Mejoras en el componente Login

--- src/Componentes/LogIn.jsx
+++ src/Componentes/LogIn.jsx
@@ -1,53 +1,49 @@
 import React, { useState } from 'react';
 import '../Estilos-de-componentes/LogIn.css'
-import { Button, CssBaseline, TextField, Box } from '@mui/material';
+import { Button, TextField, Box } from '@mui/material';
 
 export default function LogIn() {
 
-  const [correo, setCorreo] = useState('');
-  const [contraseña, setContraseña] = useState('');
+  // ORDEN
 
+  // = Hacer un funcion llamada handleSubmit que consologee el state
+  // = Hacer una validacion simple con If's para verificar que email y password tengas como minimo 5 letras 
+  // sino mostrar un alerte con un mensaje
+  // Hacer un set Local storage, buscar doc. (Tip: solo se pueden guardar Strings si necesitas guardar un Obj usar JSON.Strinfigy)
+  // Hacer un get localStorage para obtener los datos del usuario
 
-  const guardarUsuario = (e) => {
+  const initialForm = {
+    email: '',
+    password: ''
+  }
 
-    setCorreo(e.target.value);
-    setContraseña(e.target.value);
-    
-    const usuarioNuevo = {
-      
-      correo: correo,
-      contraseña: contraseña
+  const [formData, setFormData] = useState(initialForm)
 
-    };
-    
-    console.log(usuarioNuevo);
-
-    
-
-  };
-
-
+  const handleInputs = (e, field) => {
+    setFormData({
+      ...formData,
+      [field]: e.target.value
+    })
+  }
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      className='cajaPrincipal'
-    >
+    <>
+      <h2 className='titulo'>Login</h2>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        className='cajaPrincipal'
+      >
 
-      <CssBaseline />
+        <TextField onChange={(e) => handleInputs(e, 'email')} id="outlined-basic" label="Correo" variant="outlined" />
+        <TextField onChange={(e) => handleInputs(e, 'password')} id="outlined-password-input" label="Contraseña" type="password" autoComplete="current-password" />
 
-      <h1 className='titulo'>LogIn</h1>
-
-      <TextField onChange={guardarUsuario} id="outlined-basic" label="Correo" variant="outlined" />
-      <TextField id="outlined-password-input" label="Contraseña" type="password" autoComplete="current-password"/>
-
-      <Button sx={{ marginTop: 4, marginBottom: 1 }} variant="outlined">Iniciar Sesion</Button>
-      <Button sx={{ margin: 1 }} variant="text" color="secondary">Registarse</Button>
-
-    </Box>
+        <Button sx={{ marginTop: 4, marginBottom: 1 }} variant="outlined">Iniciar Sesion</Button>
+        <Button sx={{ margin: 1 }} variant="text" color="secondary">Registarse</Button>
+      </Box>
+    </>
   );
 }
